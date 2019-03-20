61bdce99b6ebaa5616f58d8ef67181c84ed8901b
ws-zavgorodny <ws.zavgorodny@gmail.com>
2019-03-20 18:24:12 +0300

    First commit

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPost = async () => {
+  const response = await jsonPlaceholder.get('/posts');
+  return {
+    type : 'FETCH_POSTS'
+  };
+};
